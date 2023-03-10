3f9b4d747e213ff069dd0e545dbe4e352922631d
anton.goloschapov <lanworm@mail.ru>
2023-03-10 08:33:56 +0300

    Добавил код

---
+++ project/Program.cs
@@ -0,0 +1,23 @@
+﻿System.Console.WriteLine("Введите слова через запятую: ");
+string[] array = Console.ReadLine().Split(',');
+
+string[] getResult(string[] arr)
+{
+    string result = "";
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3 && arr[i].Length != 0)
+        {
+            result += $"{arr[i]},";
+        }
+    }
+
+    return result.Split(',');
+}
+
+void PrintArray(string[] arr)
+{
+    System.Console.WriteLine("[" + string.Join(",", arr) + "]");
+}
+
+PrintArray(getResult(array));
